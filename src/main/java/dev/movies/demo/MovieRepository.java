f8c3e3dba89d46243918428bff349445248bb62f
Edalat Feizi <edalatfeizi921@gmail.com>
2023-05-14 11:22:58 +0330

    Initial commit

---
+++ src/main/java/dev/movies/demo/MovieRepository.java
@@ -0,0 +1,13 @@
+package dev.movies.demo;
+
+import dev.movies.demo.models.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findByImdbId(String imdbId);
+}
